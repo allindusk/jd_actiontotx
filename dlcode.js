e3b8a2eb517ec168796b594f130c0c5ee86604d4
allindusk <920535730@qq.com>
2021-01-12 16:18:42 +0800

    add jdCookie

--- dlcode.js
+++ dlcode.js
@@ -5,22 +5,27 @@ let fs = require('fs')
 let urlarr = [
   //jd通知
   'https://github.com/lxk0301/jd_scripts/raw/master/sendNotify.js',
+  //jdCookie
+  'https://github.com/lxk0301/jd_scripts/raw/master/jdCookie.js',
   //京东汽车兑换  "0 0 * * *"
   'https://github.com/lxk0301/jd_scripts/raw/master/jd_car_exchange.js',
 ]
+
 for (const urlstr of urlarr) {
   let scriptname = (/raw\/.*\/(.*\.js)/).exec(urlstr)[1]
+  console.log(urlstr)
   request.get({
-      url:urlstr,
-      timeout:3000},
+      url: urlstr,
+    },
     (err, resp, body) => {
-      if(resp.statusCode==200){
+      if(err){
+        console.log(err)
+      }else if(resp.statusCode==200){
         fs.writeFile(__dirname+`/jd_script/${scriptname}`,String(body), function (error) {
           if (error) {console.log(`写入失败 ${scriptname}`)
           }else {console.log(`写入成功 ${scriptname}`)}
         })
       }else{console.log(body)}
-      if(err){console.log(err)}
     }
   )
 }
